d18e812ec01f88ac3c43153e1b9a4babbac27563
Jinfeng Ren <me2r@hotmail.com>
2015-01-13 23:00:50 +0800

    Solve title display problem (due to tab indent)

--- sites/all/themes/meiyin/tpl/page.tpl.php
+++ sites/all/themes/meiyin/tpl/page.tpl.php
@@ -102,9 +102,7 @@
 			<div class="row pagetitle">
 				<?php print render($title_prefix); ?>
 				<?php if ($title): ?>
-				<h1 class="title" id="page-title">
-					<?php print $title; ?>
-				</h1>
+				<h1 class="title" id="page-title"><?php print $title; ?></h1>
 				<?php endif; ?>
 				<?php print render($title_suffix); ?>
 				<?php if ($breadcrumb): ?>
