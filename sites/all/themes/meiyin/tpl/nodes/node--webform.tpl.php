7b53abb877fc4355685bcf2685455e635069886c
Jinfeng Ren <me2r@hotmail.com>
2016-01-21 21:18:38 +0800

    Add Baidu Share to webform pages

---
+++ sites/all/themes/meiyin/tpl/nodes/node--webform.tpl.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Theme implementation to display a webform node.
+ */
+
+$path = "node/".$node->nid;
+$options = array('absolute' => TRUE);
+$url = url($path, $options);
+?>
+
+<div id="node-<?php print $node->nid; ?>" class="webform singlepost nodate <?php print $classes; ?> clearfix" <?php print $attributes; ?>>
+  <?php if(current_path() != $path): ?>
+    <h2><a href="<?php print $url; ?>"><?php print $node->title; ?></a></h2>
+  <?php endif; ?>
+
+  <?php if(!empty($content['links']['statistics']['#links']['statistics_counter'])): 
+    // Print the page view counter ?>
+    <div id="viewcounter"><span class="icon_wrap"><i class="fa fa-eye"></i><?php print $content['links']['statistics']['#links']['statistics_counter']['title']; ?></span></div>
+  <?php endif; ?>
+
+  <?php if(!empty($content['field_description'])): ?>
+    <div class="field-items webform-description">
+      <h4><?php print $node->field_description['und']['0']['value']; ?></h4>
+    </div>
+  <?php endif; ?>
+
+  <?php if(!empty($content['field_body'])): ?>
+    <div class="field-items webform-body">
+      <?php print render($content['field_body']); ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if(!empty($content['field_image'])): ?>
+    <div class="webform-background-wrapper" style="background:url(<?php print file_create_url($node->field_image['und']['0']['uri']); ?>) no-repeat; ">
+  <?php endif; ?>
+
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    hide($content['field_description']);
+    hide($content['field_image']);
+    print render($content);
+  ?>
+
+  <?php if(!empty($content['field_image'])): ?>
+    </div> 
+  <?php endif; ?>
+
+  <!-- Baidu Share -->
+  <div class="bdsharebuttonbox">
+    <h6>分享到：</h6>
+    <a href="#" class="bds_weixin" data-cmd="weixin"></a>
+    <a href="#" class="bds_tsina" data-cmd="tsina"></a>
+    <a href="#" class="bds_tqq" data-cmd="tqq"></a>
+    <a href="#" class="bds_sqq" data-cmd="sqq"></a>
+    <a href="#" class="bds_qzone" data-cmd="qzone"></a>
+    <a href="#" class="bds_more" data-cmd="more"></a>
+  </div>
+</div>
