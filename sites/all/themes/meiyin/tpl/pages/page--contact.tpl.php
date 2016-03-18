7b733e7383a8c22d250ec60ed5bc6eaf469e54b9
Jinfeng Ren <me2r@hotmail.com>
2016-03-18 21:34:44 +0800

    Fix no mobile menu on small screen

--- sites/all/themes/meiyin/tpl/pages/page--contact.tpl.php
+++ sites/all/themes/meiyin/tpl/pages/page--contact.tpl.php
@@ -34,9 +34,8 @@
 		<section class="headerwrap">
 			<div class="header span12">
 				<?php if ($logo): ?>
-				<a href="<?php print $front_page; ?>"
-					title="<?php print t('Home'); ?>" rel="home" class="logo"> <img
-					src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+				<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo">
+				  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
 				</a>
 				<?php endif; ?>
 
@@ -79,8 +78,8 @@
 				</nav><!-- /#navigation -->
 				
 				<div class="row mobilemenu">
-					<div class="icon-menu"></div>
-					<form id="responsive-menu" action="#" method="post">
+					<div class="icon-menu"><i class="fa fa-bars"></i></div>
+					<form id="responsive-menu" action="#" method="post" accept-charset="UTF-8">
 						<select></select>
 					</form>
 				</div>
