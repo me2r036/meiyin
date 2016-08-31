e3a18965f43382de897427361fafb86b0abddb32
Jinfeng Ren <me2r@hotmail.com>
2016-08-31 19:04:34 +0800

    Switch to Baidu Tuiguang account analytics code

--- sites/all/themes/meiyin/tpl/html.tpl.php
+++ sites/all/themes/meiyin/tpl/html.tpl.php
@@ -119,7 +119,7 @@
     <div class="qr-code-image-contact"><?php print $qr_code_vcard; ?></div>
   </div>
   <!-- Baidu Analytics starts here -->
-  <script>var _hmt=_hmt || [];(function() { var hm = document.createElement("script"); hm.src = "//hm.baidu.com/hm.js?78cfb05fe94233ea606c6c598772a4f3"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();</script>
+  <script>var _hmt=_hmt || [];(function() { var hm = document.createElement("script"); hm.src = "//hm.baidu.com/hm.js?08caffd77952fda78937241d780a2909"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();</script>
   <!-- Google Analytics starts here -->
   <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-82697966-1', 'auto'); ga('send', 'pageview');</script>
 </body>
